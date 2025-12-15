71d33a0892c83a00d20959d048c7287d4c6f58a1
Evan Kristian Pratama <if2372047@student.it.maranatha.edu>
2025-12-15 21:28:28 +0700

    Variable latihan

---
+++ javaScriptSatu.js
@@ -0,0 +1,17 @@
+const nama = "Evan";
+let umur = 22;
+let aktif = true;
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+console.log("status aktif:", aktif);
+
+// ubah nilai variabel
+
+umur = 
+aktif = false;
+
+console.log("Nama:", nama);
+console.log("Umur:", umur);
+console.log("status aktif", aktif);
+
